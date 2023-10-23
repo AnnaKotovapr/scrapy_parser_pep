14f295b8b1e5a64ba77ff47743e357b6d5ab93e1
Àííà Êîòîâà <anna.kottttt@mail.ru>
2023-10-23 18:37:13 +0300

    one

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# scrapy_parser_pep
+# Проект асинхронный парсер PEP
+
+## Автор
+- Анна Котова
+- E-mail: kotova.a.a.97@mail.ru
+- Telegram: @annkotttt
+
+##  Описание
+Скрапер собирает данные обо всех PEP документах и формирует результат двух типов:
+- pep_{datetime}.csv - список всех PEP (номер, название и статус)
+- status_summary_{datetime}.csv - сколько найдено документов в каждом статусе (статус, количество)
+
+
+## Технологии проекта
+Scrapy — популярный фреймворк для парсинга веб сайтов
+
+
+## Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone git@github.com:AnnaKotovapr/scrapy_parser_pep.git
+cd scrapy_parser_pep
+```
+Cоздать и активировать виртуальное окружение:
+```
+python3 -m venv env
+source env/bin/activate
+```
+Установить зависимости из файла requirements.txt:
+```
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## Примеры команд
+scrapy crawl pep - запуск парсера
---
+++ pep_parse/pipelines.py
@@ -0,0 +1,40 @@
+import csv
+import datetime as dt
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+FIELDS_NAME = ('Статус', 'Количество')
+RESULTS_DIR = 'results'
+TIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+TIMENOW = dt.datetime.now().strftime(TIME_FORMAT)
+FILENAME = 'status_summary_{time}.csv'
+
+
+class PepParsePipeline:
+    
+    def open_spider(self, spider):
+
+        self.results = {}
+        self.result_dir = BASE_DIR / RESULTS_DIR
+        self.result_dir.mkdir(exist_ok=True)
+
+    def process_item(self, item, spider):
+
+        pep_status = item['status']
+        if self.results.get(pep_status):
+            self.results[pep_status] += 1
+        else:
+            self.results[pep_status] = 1
+        return item
+
+    def close_spider(self, spider):
+
+        file_dir = self.result_dir / FILENAME.format(
+            time=TIMENOW)
+        with open(file_dir, mode='w', encoding='utf-8') as f:
+            writer = csv.writer(f, dialect='unix')
+            writer.writerow((FIELDS_NAME))
+            for key, values in self.results.items():
+                writer.writerow([key, values])
+            writer.writerow(['Total', sum(self.results.values())])
